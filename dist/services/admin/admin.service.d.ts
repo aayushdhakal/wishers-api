abe4ea89bbc4b8a24af7e5aea9f0898abef84614
Aayush Dhakal <aayushdhakal96@email.com>
2025-10-16 07:23:22 +0545

    feat: init api

--- dist/services/admin/admin.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { EventRepository } from '../../database/repositories/event.repository';
-import { ReminderContactListDto } from '../../modules/admin/dto';
-export declare class AdminService {
-    private readonly eventRepository;
-    constructor(eventRepository: EventRepository);
-    getUsersToContact(targetDate?: Date, eventType?: string, reminderDays?: number): Promise<ReminderContactListDto>;
-}
